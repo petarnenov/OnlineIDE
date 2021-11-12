c951d8ee5c42eb56da98265cc976332bf572380d
“Petar <petar.petrov@egt-interactive.com>
2021-11-12 18:21:13 +0200

    chore: reinit project

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -8,9 +8,23 @@ export const serveCommand = new Command()
   .command('serve [filename]')
   .description('open a file for editing')
   .option('-p,--port <number>', 'port to run server on', '1971')
-  .action((filename = 'notebook.js', options: ServeCommandOptions) => {
-    const port = parseInt(options.port);
-    const dir = path.join(process.cwd(), path.dirname(filename));
+  .action(async (filename = 'notebook.js', options: ServeCommandOptions) => {
+    try {
+      const port = parseInt(options.port);
+      const dir = path.join(process.cwd(), path.dirname(filename));
 
-    serve({ filename: path.basename(filename), port, dir });
+      await serve({ filename: path.basename(filename), port, dir });
+      console.log(
+        `Opened file ${filename}. Navigate to http://localhost:${port} to edit the file.`
+      );
+    } catch (err) {
+      if (err instanceof Error) {
+        if ((err as any).code === 'EADDRINUSE') {
+          console.error('Try running on different port. Use -p, --port <PORT>');
+        } else {
+          console.error('Serve error: ', err.message);
+        }
+      }
+      process.exit(1);
+    }
   });
--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,8 +1,17 @@
+import express from 'express';
+import cores from 'cors';
+import proxy from 'http-proxy-middleware';
+
 export interface ServeConfig {
   port: number;
   filename: string;
   dir: string;
 }
+
 export const serve = (serveConfig: ServeConfig) => {
-  console.log(serveConfig);
+  const app = express();
+  const { port, filename, dir } = serveConfig;
+  return new Promise<void>((resolve, reject) => {
+    app.listen(port, resolve).on('error', reject);
+  });
 };
